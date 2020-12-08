4f4bb286ba6387e9051ab117f684f891787bef9d
D1ver94 <75688853+D1ver94@users.noreply.github.com>
2020-12-08 19:36:57 +0100

    Adding Movement and Turnung
    Creating a ne PlayerMovement script,
    where we added Movement and Turning to the Player Game Object

---
+++ DreamZombieShooter1/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    Vector3 _Movement;
+    Animator anim;
+    Rigidbody _playerRb;
+    int floorMask;
+    float camReyLenght = 100f;
+
+    void Awaike()
+    {
+        _floorMask = LayerMask.GetMask("floor");
+        _anim = GetComponent < Animator >
+        _playerRb = GetComponent<RigitBody>();
+    }
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Move(horizontalInput, verticalInput);
+        Turning();
+
+    }
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalaized * speed * Time.deltatime;
+        playerRb.MovePosition(transform.position + movement);
+    }
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(input.mouseposition);
+        RaycastHit floorHit;
+
+        if (phisics.Raycast(camRay, out floorHit, camRayLenght, floorMask)
+        {
+            Vector3 playerToMouse = floorHit.point = transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRb.MoveRotation(newRotation);
+        }
+    }
+}
