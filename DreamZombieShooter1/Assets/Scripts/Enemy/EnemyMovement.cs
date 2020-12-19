3ac436a7eaf0c08df48709277a41a2452652454c
D1ver94 <75688853+D1ver94@users.noreply.github.com>
2020-12-19 09:55:35 +0100

    Adding scripts Enemy Controller adn players Health
    We added scripts Enemy Controller which allows our Enemy to follow the player, and Player Health which adds on a screen Health of a player.

---
+++ DreamZombieShooter1/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+}
+
---
+++ DreamZombieShooter1/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+   
+    public int startingHealth = 100; // стартовое здоровье
+    public int currentHealth;  // текущее здоровье
+    public Slider healthSlider;  // полоска здоровья
+    public Image damageImage;  // изображение повреждения
+    public AudioClip deathClip;  // аудио смерти
+    public float flashSpeed = 5; //
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    Animator anim; //
+    AudioSource playerAudio;
+    PlayerMovement playerMovement;
+
+    bool isDead;
+    bool damaged;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<PlayerMovement>();
+        currentHealth = startingHealth;
+    }
+    void Update()
+    {
+        if (damaged)
+        {
+            damageImage.color = flashColor;
+
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+
+    }
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+
+        healthSlider.value = currentHealth;
+
+        playerAudio.Play();
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            //Death();
+        }
+    }
+    void Death()
+    {
+        isDead = true;
+        anim.SetTrigger("Death");
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+        playerMovement.enabled = false;
+
+    }
+}
